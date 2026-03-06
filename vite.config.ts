bb9e5cb1d618c881f671ff69f12383f9b104a1e7
nyanlynn <mrnyanlynn.369@gmail.com>
2026-03-06 13:11:23 +0630

    feat: Add PWA manifest icons and maskable support
    Enhances the PWA manifest to include additional icon sizes and specifies maskable icons. This improves the application's appearance and integration on various devices and platforms.

--- vite.config.ts
+++ vite.config.ts
@@ -27,12 +27,14 @@ export default defineConfig(({mode}) => {
             {
               src: 'icon.svg',
               sizes: '192x192',
-              type: 'image/svg+xml'
+              type: 'image/svg+xml',
+              purpose: 'any maskable'
             },
             {
               src: 'icon.svg',
               sizes: '512x512',
-              type: 'image/svg+xml'
+              type: 'image/svg+xml',
+              purpose: 'any maskable'
             }
           ]
         }
